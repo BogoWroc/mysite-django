fbe296515ab1ac5825e2cd8796c4183fdf69f4b6
Bogumil Zebek <bogowrocpl@gmail.com>
2016-11-08 14:52:19 +0100

    Add delete author test

--- books/tests/test_models.py
+++ books/tests/test_models.py
@@ -8,57 +8,58 @@ from books.models import Author
 # Everything can be found at https://docs.djangoproject.com/el/1.10/ref/models/querysets/
 
 class CrudOperationsTest(TestCase):
-    def test_find_selected_author_stored_in_db(self):
-        # given
-        # Instance is automatically added to db
-        author = AuthorFactory(first_name="bogumil")
+    def setUp(self):
+        self.author1 = AuthorFactory(first_name="Author1", email="author1@gmail.com")
+        self.author2 = AuthorFactory(first_name="Author2", email="author2@wp.pl")
+        self.author3 = AuthorFactory(first_name="Author3", email="author3@poczta.onet.pl")
+        self.author4 = AuthorFactory(first_name="Author4", email="author4@gmail.com")
 
+    def tearDown(self):
+        Author.objects.all().delete()
+
+    def test_find_selected_author_stored_in_db(self):
         # when
         # Find author in db
-        actual = Author.objects.get(first_name="bogumil")
+        actual = Author.objects.get(first_name="Author1")
 
         # then
-        assert_that(actual).is_equal_to(author)
+        assert_that(actual).is_equal_to(self.author1)
 
     def test_that_all_authors_will_be_found(self):
         # given
-        authors = AuthorFactory.create_batch(20)
+        batch_of_additional_authors = AuthorFactory.create_batch(20)
 
         # when
         # all is translated to select * from
         actual = Author.objects.all()
 
         # then
-        assert_that(len(actual)).is_equal_to(20)
+        assert_that(len(actual)).is_equal_to(len(batch_of_additional_authors) + 4)
 
     def test_that_only_athors_with_gmail_account_will_be_found(self):
-        # given
-        author1 = AuthorFactory(email="author1@gmail.com")
-        author2 = AuthorFactory(email="author2@wp.pl")
-        author3 = AuthorFactory(email="author3@poczta.onet.pl")
-        author4 = AuthorFactory(email="author4@gmail.com")
-
         # when
         # email__contains is translated to WHERE email LIKE '%gmail.com%';
         found_authors = Author.objects.filter(email__contains="gmail.com")
 
         # then
         assert_that(len(found_authors)).is_equal_to(2)
-        assert_that(found_authors).contains(author1, author4)
-        assert_that(found_authors).does_not_contain(author2, author3)
+        assert_that(found_authors).contains(self.author1, self.author4)
+        assert_that(found_authors).does_not_contain(self.author2, self.author3)
 
     def test_that_only_athors_which_has_not_got_gmail_account_will_be_found(self):
-        # given
-        author1 = AuthorFactory(email="author1@gmail.com")
-        author2 = AuthorFactory(email="author2@wp.pl")
-        author3 = AuthorFactory(email="author3@poczta.onet.pl")
-        author4 = AuthorFactory(email="author4@gmail.com")
-
         # when
         # email__contains is translated to WHERE email NOT LIKE '%gmail.com%';
         found_authors = Author.objects.exclude(email__contains="gmail.com")
 
         # then
         assert_that(len(found_authors)).is_equal_to(2)
-        assert_that(found_authors).contains(author2, author3)
-        assert_that(found_authors).does_not_contain(author1, author4)
+        assert_that(found_authors).contains(self.author2, self.author3)
+        assert_that(found_authors).does_not_contain(self.author1, self.author4)
+
+    def test_that_all_records_were_dropped(self):
+        # when
+        Author.objects.all().delete()
+        found_authors = Author.objects.all()
+
+        # then
+        assert_that(len(found_authors)).is_equal_to(0)
