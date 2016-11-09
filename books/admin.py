b4bb8adcdfc327dd1ac2b625d2dbbc88e6e88909
Bogumil Zebek <bogowrocpl@gmail.com>
2016-11-09 09:53:40 +0100

    Customize authors list at admin page

--- books/admin.py
+++ books/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 
 from .models import Author, Book, Publisher
 
+
+# Customize authors list at admin page - replace single columnt(default __str__) by three columns
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+
+
 # Register your models here.
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book)
 admin.site.register(Publisher)
