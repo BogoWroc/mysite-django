61da581915debc4ec55712c05c7a1b6a751c86a6
Bogumil Zebek <bogowrocpl@gmail.com>
2016-11-09 09:55:47 +0100

    Add search field to author list at admin page

--- books/admin.py
+++ books/admin.py
@@ -6,6 +6,7 @@ from .models import Author, Book, Publisher
 # Customize authors list at admin page - replace single columnt(default __str__) by three columns
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name')
 
 
 # Register your models here.
