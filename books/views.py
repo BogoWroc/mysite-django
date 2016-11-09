46ce0ced08386017f2f5809262616e4b23504241
Bogumil Zebek <bogowrocpl@gmail.com>
2016-11-09 10:45:31 +0100

    Add view which display browser version as a response

--- books/views.py
+++ books/views.py
@@ -1,1 +1,7 @@
 # Create your views here.
+from django.http import HttpResponse
+
+
+def browser_information(request):
+    ua = request.META.get('HTTP_USER_AGENT', 'unknown')
+    return HttpResponse("Your browser is %s" % ua)
