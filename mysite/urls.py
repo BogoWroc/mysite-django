a1a79a221536a1cb94394789e16ead55ebf56a0f
Bogumil Zebek <bogowrocpl@gmail.com>
2016-11-25 21:08:22 +0100

    Add web map for web page indexing

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 
 from basic.views import current_datetime, hello, hours_ahead, order_report, contact
+from blog.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts': PostSitemap,
+}
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -26,5 +32,18 @@ urlpatterns = [
     url(r'^order-report/$', order_report),
     url(r'^books/', include('books.urls')),
     url(r'^contact/$', contact),
-    url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
+    url(
+        r'^blog/',
+        include(
+            'blog.urls',
+            namespace='blog',
+            app_name='blog'
+        )
+    ),
+    url(
+        r'^sitemap\.xml$',
+        sitemap,
+        {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap'
+    ),
 ]
